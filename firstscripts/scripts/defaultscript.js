660d43c6588343fe17e3e6be104fddcb1bd499be
LaMarque Drew <ldrew1@uncc.edu>
2022-09-23 21:10:51 -0400

    index

---
+++ firstscripts/scripts/defaultscript.js
@@ -0,0 +1,21 @@
+function displayDate() {
+document.getElementById("showDate").innerHTML = Date();
+}
+function greet(Name, feeling){
+	alert("Drew's Dogs welcomes you, " + Name.value +" you feel "+ feeling.value) + "!";
+}
+function fact1(){
+	alert("There are 339 breeds of dogs! That's a lot!");
+}
+function fact2(){
+	alert("The average lifespan of a dog is 10-13 years. Better give them all the love you can!");
+}
+function fact3(){
+	alert("The biggest (heaviest) dog breed is the English Mastiff, which can weigh as much as 200 pounds! Whoa!");
+}
+function fact4(){
+	alert("The smallest dog breed is the Chuihuaha. Fully grown adults can weigh 3-6 pounds.");
+}
+function fact5(){
+	alert("If you want to make your dog really happy, give them lots of treats and belly rubs");
+}
